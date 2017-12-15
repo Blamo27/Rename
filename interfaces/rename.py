fa40c85f57d8974a9e6482ddacb2c84344af5fbf
Nems27 <31938708+Nems27@users.noreply.github.com>
2017-12-15 21:34:25 +0100

    Update rename.py

--- interfaces/rename.py
+++ interfaces/rename.py
@@ -37,5 +37,38 @@ def tkTemp(self):
 
     Label(root, text = "Extension concernée").grid(row = 4; column = 4);
     self.extension = Entry(root).grid(row = 5, column = 4);
-
+    
+    root.mainloop()
     # RULE & UTILS 
+    
+def tkRules(self):
+    root = Tk()
+    root.title("Ajout de règle")
+
+    List = Button(root, text="Lister").grid(row = 0, column = 0, stick = W)
+    CreateButton = Button(root, text="Créer").grid(row = 1, column = 0, stick = W)
+
+    Label(root, text="Créer une règle").grid(row=1, column=2)
+    Label(root, text = "Nom de la règle").grid(row = 2, column = 1)
+    Entry(root).grid(row = 2, column = 2, stick = W)
+
+    Label(root, text = "Amorce").grid(row = 4, column = 0, stick = W)
+    none = Button(root, text="Aucune", command=lambda :self.init).grid(row=5, column=0, stick=W)
+    Letter = Button(root, text="Lettre", command=lambda :self.init).grid(row=6, column=0, stick=W)
+    Number = Button(root, text="Chiffre", command=lambda :self.init).grid(row=7, column=0, stick=W)
+
+    Label(root, text = "A partir de").grid(row = 8, column = 0, stick = W)
+    start = Entry(root).grid(row = 9, column = 0,stick = W)
+
+    Label(root, text = "Préfixe").grid(row = 4, column = 1)
+    prefix = Entry(root).grid(row = 5, column = 1)
+
+    originalname = Checkbutton(root, text="Nom original").grid(row=5, column=2)
+
+    Label(root, text = "Postfixe").grid(row = 4, column = 3)
+    post = Entry(root).grid(row = 5, column = 3)
+
+    Label(root, text = "Extension concernée").grid(row = 4, column = 4);
+    self.extension = Entry(root).grid(row = 5, column = 4);
+
+    root.mainloop()
